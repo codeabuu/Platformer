30662991105af39e1067a50fe23234327250f064
codeabuu <abdulngui718@gmail.com>
2024-04-28 19:03:22 -0400

    set sprites, directions

--- game.py
+++ game.py
@@ -18,8 +18,39 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+
+
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255,0,0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
@@ -27,6 +58,7 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
         self.direction = "left"
         self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -47,9 +79,14 @@ class Player(pygame.sprite.Sprite):
 
     def loop(self, fps):
         self.move(self.x_vel, self.y_vel)
+        #self.y_vel += min(1, (self.fall_count / FPS) * self.GRAVITY)
+
+        self.fall_count += 1
+
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 def get_background(name):
     image = pygame.image.load(join('assets', 'Background', name))
@@ -71,6 +108,15 @@ def draw(window, background, bg_image, player):
 
     pygame.display.update()
 
+def handle_move(player):
+    key = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if key[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if key[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
@@ -85,6 +131,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        player.loop(FPS)    
+        handle_move(player)
         draw(window, background, bg_image, player)
 
     pygame.quit()
